3c2f026f592e10e14bf0e86bf875760f4888f2a1
pradyumn8 <96361733+pradyumn8@users.noreply.github.com>
2024-11-13 10:05:39 +0530

    About & program section is almost complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
+import Programs from './components/Programs/Programs'
+import Title from './components/Title/Title'
+import About from './components/About/About'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,11 @@ function App() {
   <div>
     <Navbar/>
     <Hero/>
+    <div className="container">
+      <Title subTitle='Our Program' title='What We Offer'/>
+      <Programs/>
+      <About/>
+    </div>
   </div>
   )
 }
