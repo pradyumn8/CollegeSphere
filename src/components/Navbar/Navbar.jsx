28e8edaf8db495949b5c426fba846d62a8d33fe4
pradyumn8 <96361733+pradyumn8@users.noreply.github.com>
2024-11-13 13:11:41 +0530

    using media queries for responsiveness

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
 import { Link } from 'react-scroll'
+import menu_icon from '../../assets/menu-icon.png'
 
 
 const Navbar = () => {
@@ -13,10 +14,14 @@ const Navbar = () => {
     })
   }, [])
 
+  const [mobileMenu,setMobileMenu] = useState(false)
+const toggleMenu = ()=>{
+  mobileMenu ? setMobileMenu(false) : setMobileMenu(true)
+}
   return (
     <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
       <img src={logo} alt="" className='logo' />
-      <ul>
+      <ul className={mobileMenu?'':'hide-mobile-menu'}>
         <li>
           <Link to="hero" smooth={true} offset={0} duration={500}>
             Home
@@ -48,6 +53,7 @@ const Navbar = () => {
           </Link>
         </li>
       </ul>
+      <img src={menu_icon} className='menu-icon' onClick={toggleMenu} alt="" />
     </nav>
   )
 }
