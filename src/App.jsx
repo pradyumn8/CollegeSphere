76c560b5ce63271614872b2baa2b34e0233531e2
pradyumn8 <96361733+pradyumn8@users.noreply.github.com>
2024-11-13 12:08:53 +0530

    install react scroll and add on navbar

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import About from './components/About/About'
 import Campus from './components/Campus/Campus'
 import Testimonials from './components/Testimonials/Testimonials'
 import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -26,6 +27,7 @@ function App() {
       <Testimonials/>
       <Title subTitle='Contact Us' title='Get in Touch'/>
       <Contact/>
+      <Footer/>
     </div>
   </div>
   )
